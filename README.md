e990b64a95a15f2156bb0cb214950c53231bb00a
Koa Sato <koa.sato@gmail.com>
2021-05-15 12:38:54 -0700

    Basic bot working with command $hello. Created bot through GUI interface and can log in using token.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # discord_music_bot
+
+####Create a config.env with the following:
+#!/bin/bash
+
+export OAUTH2_CLIENT_ID="XXXXXXXXXXXXXXXXXX"
+export OAUTH2_CLIENT_SECRET="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+export TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+#### Then, run config.sh to export the env variables
+. config.env
---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+
+    # Ignore messages sent by the bot itself to prevent infinite loops
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.getenv('TOKEN'))
